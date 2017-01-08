54789692832d7cb9c2ff6d9b968c1956394b5217
chaharu <chaharu@matrix.co.il>
2017-01-08 13:05:32 +0200

    added the api tests to v2x

--- embedded/sdk/sdk-4.x/common/link/link.h
+++ embedded/sdk/sdk-4.x/common/link/link.h
@@ -11,6 +11,7 @@ int cli_v2x_link_socket_delete( struct cli_def *cli, const char *command, char *
 
 int cli_v2x_link_service_create( struct cli_def *cli, const char *command, char *argv[], int argc );
 int cli_v2x_link_service_delete( struct cli_def *cli, const char *command, char *argv[], int argc );
+int cli_test_v2x_link_service_get (struct cli_def *cli, const char *command, char *argv[], int argc); // chrub
 
 int cli_v2x_link_tx( struct cli_def *cli, const char *command, char *argv[], int argc );
 int cli_v2x_link_rx( struct cli_def *cli, const char *command, char *argv[], int argc );
@@ -21,4 +22,19 @@ int cli_v2x_link_print_cntrs( struct cli_def *cli, const char *command, char *ar
 int cli_v2x_get_link_socket_addr( struct cli_def *cli, const char *command, char *argv[], int argc );
 int cli_v2x_set_link_socket_addr( struct cli_def *cli, const char *command, char *argv[], int argc );
 
+int cli_test_v2x_link_dot4_channel_start( struct cli_def *cli, const char *command, char *argv[], int argc );   // chrub
+int cli_test_v2x_link_dot4_channel_end( struct cli_def *cli, const char *command, char *argv[], int argc );   // chrub
+int cli_test_v2x_link_dot4_channel_end_receive( struct cli_def *cli, const char *command, char *argv[], int argc );   // chrub
+
+//int cli_test_v2x_link_netif_profile_set( struct cli_def *cli, const char *command, char *argv[], int argc );   // chrub - not checked (Zohar's requirement)
+int cli_test_v2x_link_send( struct cli_def *cli, const char *command, char *argv[], int argc );   // chrub
+
+int cli_test_v2x_link_receive( struct cli_def *cli, const char *command, char *argv[], int argc ); //chrub
+int cli_test_v2x_link_socket_create( struct cli_def *cli, const char *command, char *argv[], int argc );
+
+//int cli_test_v2x_link_sample_subscriber_create( struct cli_def *cli, const char *command, char *argv[], int argc ); //chrub - not checked (Zohar's requirement)
+
+//int cli_test_v2x_link_sample_subscriber_delete( struct cli_def *cli, const char *command, char *argv[], int argc );//chrub - not checked (Zohar's requirement)
+
+//int cli_test_v2x_link_sample_int32_receive( struct cli_def *cli, const char *command, char *argv[], int argc );//chrub - not checked (Zohar's requirement)
 #endif /* __ATE_V2X_CLI_LINK_H__ */
